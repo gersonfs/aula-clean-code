b0aa02a586db02b8ed7d5b0c56cb04f9666de283
Gerson Felipe Schwinn <gerson@onehost.com.br>
2021-09-21 19:24:32 -0300

    Adicionando mais testes

--- tests/Services/CpfValidator.test.ts
+++ tests/Services/CpfValidator.test.ts
@@ -9,3 +9,24 @@ test("CPF vazio", () => {
     let service = new CpfValidatror();
     expect(service.validate("")).toBeFalsy();
 });
+
+
+test("CPF valor inválido", () => {
+    let service = new CpfValidatror();
+    expect(service.validate("abc12345678")).toBeFalsy();
+});
+
+test("CPF valor inválido digitos iguais", () => {
+    let service = new CpfValidatror();
+    expect(service.validate("11111111111")).toBeFalsy();
+});
+
+test("CPF válido somente números", () => {
+    let service = new CpfValidatror();
+    expect(service.validate("11234392062")).toBeTruthy();
+});
+
+test("CPF válido com pontuação", () => {
+    let service = new CpfValidatror();
+    expect(service.validate("945.207.120-68")).toBeTruthy();
+});
